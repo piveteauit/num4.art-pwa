eaa3a1eabb34b4da977120f7ab285bc3424ce7d3
piveteauit <130970296+piveteauit@users.noreply.github.com>
2025-01-07 21:26:56 +0100

    Update route.ts

--- app/api/upload/song/route.ts
+++ app/api/upload/song/route.ts
@@ -3,7 +3,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 export const runtime = "nodejs"
 
 
